1169afa462be9d237bde47e10efd5c8b999a13e2
Damien <damien.vaiton01@gmail.com>
2024-12-26 23:38:10 +0100

    Initial Commit

---
+++ TestSwift/TestSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftApp.swift
+//  TestSwift
+//
+//  Created by LalemantFrance Web on 26/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
